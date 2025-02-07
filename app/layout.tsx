f1f3abe072edd578bc292b8f0796669158ea8664
Yassino14 <louatiyassino8b4@gmail.com>
2025-02-07 18:23:50 +0100

    change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,23 +1,16 @@
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { Providers } from "@/components/providers"
-import Navbar from "@/components/Navbar"
-import type React from "react" // Added import for React
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Providers } from "@/components/providers";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Yassino",
-  description: "A showcase of my work and skills",
-  icons: {
-    icon : '/log.jpg',
-  },
-}
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+  icons: { icon: "/pp.png" },
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-[#0a192f] text-[#8892b0] min-h-screen flex flex-col`}>
@@ -27,6 +20,5 @@ export default function RootLayout({
         </Providers>
       </body>
     </html>
-  )
+  );
 }
-
