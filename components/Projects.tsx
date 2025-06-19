dd4df70e067960af441960eb7407215e53ac5003
Yassino14 <louatiyassino8b4@gmail.com>
2025-06-19 13:20:11 +0100

    Resolve merge conflicts

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -6,7 +6,7 @@ import { motion, AnimatePresence } from "framer-motion"
 import { X } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent } from "@/components/ui/card"
-import { FaGithub } from "react-icons/fa"
+import { FaGithub,FaLink, FaYoutube } from "react-icons/fa"
 
 type Project = {
   title: string
